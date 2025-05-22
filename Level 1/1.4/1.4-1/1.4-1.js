1a50238e51e1a22d3a09ccc7e9e42c8346d0dfc4
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-05-22 19:59:38 +0300

    modifed

---
+++ Level 1/1.4/1.4-1/1.4-1.js
@@ -0,0 +1,9 @@
+// Выведите в консоль все целые числа от 1 до 100.
+
+function result() {
+    for (let i = 1; i < 100 + 1; i++) {
+        console.log(i)
+    }
+}
+
+result();
---
+++ Level 1/1.4/1.4-2/1.4-2.js
@@ -0,0 +1,12 @@
+// Выведите в консоль все целые числа от -100 до 0.
+
+
+function result() {
+
+    for(let i = -100; i < 0; i++) {
+        console.log(i)
+    }
+
+}
+
+result();
